b43bc4c49116495508e35e00c66a2c2364adc918
Reza Panahi <rezabumanpanahi@gmail.com>
2026-08-09 10:42:54 +0430

    Scan service added

---
+++ src/modules/attendance/controller.js
@@ -0,0 +1,11 @@
+// Scan controller
+// req: token from body
+// res: success, status, message, and data returned from service
+
+// get All Attendace Controller
+// req: Quary parameters
+// as previous module
+
+// get to day attendace
+// req: just call the service and store
+// res: success, status, message, and data returned from service
--- src/modules/attendance/service.js
+++ src/modules/attendance/service.js
@@ -1,7 +1,56 @@
+import Attendance from "./model.js";
+import Employee from "../employee/model.js";
+import throwError from "../../utils/throwError.js";
+import crypto from "node:crypto";
+
 // Scan Service
-// Input: Token
+// Input: Token generated from reading QR code
 //flow:Hash toke -> fine employee with hashedtoken -> check employee existence -> check if employee is active -> check attendace existence with data: if not exist put checkin and chekcout to null, if exist and checkout == to null put checkout, if not null error
 // output: employee basic details + attendance details
+const scanService = async (token) => {
+    const tokenHash = crypto.createHash("sha256").update(token).digest("hex");
+
+    const employee = await Employee.findOne({
+        where: {QRcodeTokenHash: tokenHash}
+    });
+
+    if (!employee) throwError("Employee not found", 400);
+    if (employee.isActive === false) throwError("Employee is deactivated", 400);
+
+    const currentDateTime = new Date();
+    const today = currentDateTime.toISOString().split("T")[0];
+
+    const todayAttendance = await Attendance.findOne({
+        where: {
+            employeeId: employee.id,
+            date: today
+        }
+    });
+
+    if (!todayAttendance) {
+        attendance = await Attendance.create({
+            employeeId: employee.id,
+            date: today,
+            checkInTime: currentDateTime,
+            checkOutTime: null
+        });
+    } else if (todayAttendance.checkOutTime === null) {
+        await todayAttendance.update({
+            checkOutTime: currentDateTime
+        });
+    } else {
+        throwError("Employee has already checked out today", 400);
+    }
+
+    return {
+        employeeId: employee.id,
+        fullName: `${employee.firstName} ${employee.lastName}`,
+        date: attendance.date,
+        checkInTime: attendance.checkInTime,
+        checkOutTime: attendance.checkOutTime,
+        action: attendance.checkOutTime ? "checked-out" : "checked-in"
+    };
+};
 
 
 
